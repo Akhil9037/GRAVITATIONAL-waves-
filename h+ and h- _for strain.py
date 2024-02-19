1cfbace7d1d9fc752c9098d5ea964c6f15ae4bb0
Akhil9037 <147274367+Akhil9037@users.noreply.github.com>
2024-02-19 22:01:28 +0530

    Add files via upload

--- h+ and h- _for strain.py
+++ h+ and h- _for strain.py
@@ -7,7 +7,7 @@ def h(t,r1,th,ph,ph1,ph2):
     
     a11=mu/(2*D)
     a1=(1-cos(2*th)*cos**2(ph(t))-3*cos(2*ph(t)))*r1(t)**2
-    a2=(3+cos(2*th))*(2*cos(2*ph(t))*ph1(t)**2+sin(2*ph(t))*ph2(t))*(r(t)**2)
+    a2=(3+cos(2*th))*(2*cos(2*ph(t))*ph1(t)**2+sin(2*ph(t))*ph2(t))*(r(t)**2)                           # a11*a1*a2*a3*a4*a5= eqn 7 in miller pdf for plus polarization 
     a3=4*(3+cos(2*th))*(sin(2*ph(t))*ph1(t)*r1(t))
     a4=(1-2*cos(2*th)*cos(ph(t))**2-3*cos(2*ph(t)))*r2(t)
     a5=r(t)
@@ -17,15 +17,16 @@ def h(t,r1,th,ph,ph1,ph2):
     b1=(-2*mu*cos(th))/D
     b2=sin(2*ph(t))*r1(t)**2
     b3=(cos(2*ph(t))*ph2(t)-2*sin(2*ph(t))*ph2(t))*r(t)
-    b4=(4*cos(2*ph(t))*ph1(t)*r1(t)+sin(2*ph(t))*r2(t))*r(t)
-    B=b1*(b2+b3+b4)
+    b4=(4*cos(2*ph(t))*ph1(t)*r1(t)+sin(2*ph(t))*r2(t))*r(t)                                            #b1*b2*b3*b4= eqn 8 in miller pdf for cross polarization
+    
+    B=b1*(b2+b3+b4)                                                                                     # Lo=eqn 10 in miller pdf    angular momntum       ,     a=eqn 5 in miller pdf semi major axis 
 
-    LO=(G*M*mu*a*(1-e**2))**(.5)
+    LO=(G*M*mu*a*(1-e**2))**(.5)                                                                        # r=eqn 12 in miller pdf distance between BBh's    ,     
 
     L=LO/mu
-    a=(pob**2(G*(m1+m2)/4*3.14**2))**(1/3)
+    a=(pob**2(G*(m1+m2)/4*3.14**2))**(1/3)                    #Vt= potential eqn 15  in miller pdf ,  phi= eqn 16 in miller pdf   , co=eqn 14 in miller pdf    ,  E and L =eqn 11 in miller pdf
     r=a*(1-e**2)/(1+e*cos(py))
-    Vt=E*r**4/(r**2-2*r)
+    Vt=E*r**4/(r**2-2*r)                                     # mu reduced mass ,  m1 =m2 = solar mass , G= the gra.const c= light spped  ,    Eo=eqn 9S in miller pdf
     ph1=L/Vt
 
 
